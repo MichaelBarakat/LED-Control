338a3fda20483baf5b8c27c3a88a2ff6e8ca761b
Michael Barakat <MichaelBarakat2014@Gmail.com>
2022-08-26 15:17:13 +0200

    Revert "Revert "deleted unnecessary files""
    This reverts commit 2b6b266263a77f64d52b9a381003b47e451284f6.

--- Keil/Objects/main.d
+++
@@ -1,3 +0,0 @@
-./objects/main.o: ..\Source\App\Main.c ..\Source\Common\Std_Types.h \
-  ..\Source\Common\Bit_Math.h ..\Source\MCAL\Include\Timer.h \
-  ..\Source\Common\platform_Types.h
--- Keil/Objects/timer.d
+++
@@ -1,3 +0,0 @@
-./objects/timer.o: ..\Source\MCAL\Timer.c ..\Source\Common\Std_Types.h \
-  ..\Source\Config\Timer_Cfg.h ..\Source\Common\platform_Types.h \
-  ..\Source\MCAL\Include\Timer.h ..\Source\Common\mcu_hw.h
